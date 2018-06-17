a6754b65c01513dd14e0872bf039de0400064a3a
Alberto Ochoa <ochoapublic@gmail.com>
2018-06-17 23:21:17 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,6 +3,6 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
-def introudction_with_language(name, language)
+def introdction_with_language(name, language)
   puts "Hi, my name is #{name} an I am learning to program in #{language}"
 end
